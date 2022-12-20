692fe122af26305151c23c4ac4b833411de805cc
anilcand <anilcan.demirtas@smartera.com.tr>
2022-12-20 14:37:50 +0300

    Update file path

---
+++ movie-catalog-service/src/main/java/com/anilcand/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,47 @@
+package com.anilcand.moviecatalogservice.resources;
+
+import com.anilcand.moviecatalogservice.models.CatalogItem;
+import com.anilcand.moviecatalogservice.models.UserRating;
+import com.anilcand.moviecatalogservice.services.MovieInfo;
+import com.anilcand.moviecatalogservice.services.UserRatingInfo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    private final RestTemplate restTemplate;
+    private final UserRatingInfo userRatingInfo;
+    private final MovieInfo movieInfo;
+
+    public MovieCatalogResource(RestTemplate restTemplate, UserRatingInfo userRatingInfo, MovieInfo movieInfo) {
+        this.restTemplate = restTemplate;
+        this.userRatingInfo = userRatingInfo;
+        this.movieInfo = movieInfo;
+    }
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        UserRating userRating = userRatingInfo.getUserRating(userId);
+
+        return userRating.getUserRatings().stream().map(movieInfo::getCatalogItem).
+                collect(Collectors.toList());
+    }
+}
+
+
+/* Alternative way to call api
+Movie movie = webClientBuilder.build()
+                        .get()
+                        .uri("localhost:8082/movies/" + rating.getMovieId())
+                        .retrieve()
+                        .bodyToMono(Movie.class)
+                        .block();
+*/
---
+++ ratings-data-service/src/main/java/com/anilcand/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,24 @@
+package com.anilcand.ratingsdataservice.resources;
+
+import com.anilcand.ratingsdataservice.models.Rating;
+import com.anilcand.ratingsdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingsResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 5);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+    }
+}
